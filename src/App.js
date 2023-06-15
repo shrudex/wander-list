14724fcdd5ada5c14f40f11f500f34acbe98e9c6
shrudex <codeshubh@gmail.com>
2023-06-15 20:30:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [c, setC] = useState(0);
+  const [s, setS] = useState(0);
+  const currentDate = new Date();
+  const today = currentDate.toDateString();
+  currentDate.setDate(currentDate.getDate() + c);
+  const nextDay = currentDate.toDateString();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="hordiv">
+        <button onClick={()=> s>0 ? setS((s)=>s-1) : null}>-</button>
+        <p>Step : {s}</p>
+        <button onClick={()=> setS((s)=>s+1)}>+</button>
+      </div>
+      
+      <div className="hordiv">
+        <button onClick={()=> setC((c)=>c-s)}>-</button>
+        <p>Count : {c}</p>
+        <button onClick={()=> setC((c)=>c+s)}>+</button>
+      </div>
+
+      <div className="hor">
+        <p>Today is {today}</p>
+      </div>
+
+      <div className="ans">
+        <p>{c} days from today is {nextDay}</p>
+      </div>
+
+    </>
   );
 }
 
