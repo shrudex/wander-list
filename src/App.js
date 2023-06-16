dd2e74548e0cf68b849c70b31e0b1932a2f64eac
shrudex <codeshubh@gmail.com>
2023-06-16 23:38:29 +0530

    footer and form text

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div>
       <Logo />
+      <Form />
+      <List />
+      <Stats />
     </div>
   );
 }
@@ -21,29 +24,43 @@ function Logo() {
       <p
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        style={{ color: !isHovered ? "#fff500" : "#fff" }}
+        style={{ color: !isHovered ? "#fff500" : "#F1F6F9" }}
       >
         -
       </p>
       <h1
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        style={{ color: isHovered ? "#fff500" : "#fff" }}
+        style={{ color: isHovered ? "#fff500" : "#F1F6F9" }}
       >
         WanderList
       </h1>
       <p
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        style={{ color: !isHovered ? "#fff500" : "#fff" }}
+        style={{ color: !isHovered ? "#fff500" : "#F1F6F9" }}
       >
         -
       </p>
     </div>
   );
 }
-function Form() {}
-function List() {}
-function Stats() {}
+function Form() {
+  return <div className="form">
+    <p>What do you need for your ✨ trip?</p>
+    
+  </div>;
+}
+function List() {
+  return <div className="list"></div>;
+}
+function Stats() {
+  return (
+    <div className="stats">
+      <span class="material-symbols-outlined">airplane_ticket</span>
+      <p>You have 6 items on your list, and you already packed 0 (0%)</p>
+    </div>
+  );
+}
 
 export default App;
